e16269f43cdd769cce98707dd7078b2d8f631d64
zkc-xyz1009 <54833907+zkc-xyz1009@users.noreply.github.com>
2022-05-12 16:24:09 +0800

    Update func2.m

--- func2.m
+++ func2.m
@@ -1,9 +1,9 @@
-function result = func2(x,y)
+function result = func2(x)
 x1=0;
 x2=0;
 for i=1:5
-    x1=x1+i*cos((i+1)*x+i);
-    x2=x2+i*cos((i+1)*y+i);
+    x1=x1+i*cos((i+1)*x(1)+i);
+    x2=x2+i*cos((i+1)*x(2)+i);
 end
 fit=x1*x2;
 result =fit;
